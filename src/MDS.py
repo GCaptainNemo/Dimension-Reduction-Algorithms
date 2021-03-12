33dc84eb408ec389128537038eb32c1b8395f946
GCaptainNemo <1136003685@qq.com>
2021-03-12 14:28:01 +0800

    add MDS experiment

--- src/MDS.py
+++ src/MDS.py
@@ -31,54 +31,84 @@ class MDS:
             self.Y_data = np.concatenate([y, new_y], axis=0)
             print(self.X_data.shape)
         else:
-            self.X_data, self.Y_data = make_moons(100, noise=.04, random_state=0)
-            # self.X_data = self.X_data - np.mean(self.X_data, 0)
+            self.X_data, self.Y_data = make_moons(50, noise=.04, random_state=0)
 
     def MDS(self):
-        mat = np.mat(self.X_data).T
-        centeralized_mat = mat - np.mean(mat, 1)
-        u, sigma, v = np.linalg.svd(centeralized_mat)
-        self.Projective_matrix = u[:, 0:self.reduced_dimension]
-        print(self.Projective_matrix)
-        self.new_mat = self.Projective_matrix.T @ centeralized_mat
+
+        self.D = self.construct_distance_matrix()
+        self.B = self.construct_innerprod_matrix()
+        # A是对角阵，Q是特征向量矩阵
+        # 注意可能由于数值精度产生虚数，可以直接取实部计算
+        A, Q = np.linalg.eig(self.B)
+        Qk = Q[:, :self.reduced_dimension].real
+        Ak = np.diag(A[:self.reduced_dimension].real ** 0.5)
+        print(Ak)
+        print(Qk)
+        self.new_data = Qk @ Ak
+        print(self.new_data.shape)
+
+    def construct_distance_matrix(self, distance=None):
+        length = self.X_data.shape[0]
+        distance_matrix = np.zeros([length, length])
+        if distance == None:
+            dis = lambda x, y: np.linalg.norm(x-y)
+        else:
+            dis = distance
+        for i in range(length):
+            for j in range(i, length):
+                distance_matrix[i, j] = dis(self.X_data[i, :], self.X_data[j, :])
+                distance_matrix[j, i] = distance_matrix[i, j]
+        return distance_matrix
+
+    def construct_innerprod_matrix(self):
+        innerprod_matrix = np.zeros(self.D.shape)
+        length = self.D.shape[0]
+        meandist2 = np.mean(list(map(lambda x: x**2, self.D)))
+        meandist2_column_lst = []
+
+        for j in range(length):
+            meandist2_column_lst.append(np.mean(list(map(lambda x: x**2,
+                                                         self.D[:, j]))))
+        for i in range(length):
+            meandist2_i_row = np.mean(list(map(lambda x: x**2, self.D[i, :])))
+            for j in range(i, length):
+                meandist2_j_column = meandist2_column_lst[j]
+                innerprod_matrix[i, j] = -0.5 * (self.D[i, j] ** 2 - meandist2_i_row -
+                                                 meandist2_j_column + meandist2)
+                innerprod_matrix[j, i] = innerprod_matrix[i, j]
+        return innerprod_matrix
 
     def result(self, regression=False):
-        if regression == False:
-            translate_vector = [- self.Projective_matrix[1, 0], self.Projective_matrix[0, 0]]
-        else:
-            translate_vector = [- self.Projective_matrix[1, 0] * 100, self.Projective_matrix[0, 0] *100]
-
-        for i in range(self.new_mat.shape[1]):
+        for i in range(self.new_data.shape[0]):
             if self.Y_data[i] == 1:
-                # plt.scatter(self.new_mat[0, i], 0, c='r')
-                plt.scatter(self.new_mat[0, i] * self.Projective_matrix[0, 0] + translate_vector[0],
-                            self.new_mat[0, i] * self.Projective_matrix[1, 0] + translate_vector[1], c='r')
+                # plt.scatter(self.new_data[i, 0], 0, c='r')
+                plt.scatter(self.new_data[i, 0], self.new_data[i, 1], c='r')
             else:
-                # plt.scatter(self.new_mat[0, i], 0, c='b')
-                plt.scatter(self.new_mat[0, i] * self.Projective_matrix[0, 0] + translate_vector[0],
-                            self.new_mat[0, i] * self.Projective_matrix[1, 0] + translate_vector[1], c='b')
-        one_index = np.where(self.Y_data == 1)
-        zero_index = np.where(self.Y_data == 0)
-        plt.scatter(self.X_data[one_index, 0], self.X_data[one_index, 1], c='r')
-        plt.scatter(self.X_data[zero_index, 0], self.X_data[zero_index, 1], c='b')
+                # plt.scatter(self.new_data[i, 0], 0, c='b')
+                plt.scatter(self.new_data[i, 0], self.new_data[i, 1], c='b')
+
+        # one_index = np.where(self.Y_data == 1)
+        # zero_index = np.where(self.Y_data == 0)
+        # plt.scatter(self.X_data[one_index, 0], self.X_data[one_index, 1], c='r')
+        # plt.scatter(self.X_data[zero_index, 0], self.X_data[zero_index, 1], c='b')
         ax = plt.gca()
         ax.axis("equal")
-        ax.annotate("",
-                    xy=(translate_vector[0], translate_vector[1]),
-                    xytext=(0, 0),
-                    # xycoords="figure points",
-                    arrowprops=dict(arrowstyle="->", color="k"))
-        if regression:
-            plt.text(translate_vector[0] * 0.9, translate_vector[1] * 0.95 + 10,
-                     r'projection', fontdict={'size': 8, 'color': 'k'})
-        else:
-            plt.text(translate_vector[0] * 0.95 + 0.05, translate_vector[1] * 0.95,
-                     r'projection', fontdict={'size': 8, 'color': 'k'})
+        # ax.annotate("",
+        #             xy=(translate_vector[0], translate_vector[1]),
+        #             xytext=(0, 0),
+        #             # xycoords="figure points",
+        #             arrowprops=dict(arrowstyle="->", color="k"))
+        # if regression:
+        #     plt.text(translate_vector[0] * 0.9, translate_vector[1] * 0.95 + 10,
+        #              r'projection', fontdict={'size': 8, 'color': 'k'})
+        # else:
+        #     plt.text(translate_vector[0] * 0.95 + 0.05, translate_vector[1] * 0.95,
+        #              r'projection', fontdict={'size': 8, 'color': 'k'})
         plt.show()
 
 
 if __name__ == "__main__":
-    a = MDS(1)
+    a = MDS(2)
     a.make_data()
     a.MDS()
     a.result()
