09b7cdeb7b40e781fd9449a76b4f3e54366144eb
GCaptainNemo <1136003685@qq.com>
2021-03-14 13:39:29 +0800

    finish KPCA

--- docs/KPCA.md
+++ docs/KPCA.md
@@ -3,6 +3,8 @@
 KPCA是PCA加上核技术，解决某些线性不可分的数据在用PCA投影之后不同类的数据会发生交叠，导致后面分类器精度降低的问题。
 
 ## 二、原理
+
+### 2.1 算法原理
 核技术就是将当前空间线性不可分的数据X通过映射φ映射到高维空间(又称为特征空间 feature space)进而线性可分。而KPCA实质上就是在特征空间(feature space)中对数据使用PCA。更具体一点，
 [PCA](PCA.md)是要计算原空间中数据的协方差矩阵1/N * XX<sup>T</sup>，然后进行特征值分解；而KPCA就是要计算特征空间中数据的协方差矩阵C=1/N * φ(X)φ(X)<sup>T</sup>并进行特征值分解(假设矩阵X和φ(X)每行的均值均为0)。
 
@@ -34,3 +36,42 @@ K*归一化特征向量为u，则协方差矩阵C特征向量为φ(X)u。
 2. 算出中心化核矩阵K*的特征向量和特征值，取较大的k个特征值对应的k个单位特征向量u1, u2, ..., un。
 3. 算出特征空间投影坐标 vi (i=1,2,...,k)。
 ```
+
+### 2.2 常用核函数
+
+#### 1. 径向基函数核(Radial Basis Function)
+径向基函数核(Radial Basis Function, RBF kernel)，也被称为高斯核(Gaussian kernel)
+
+![RBF-kernel](../resources/KPCA/RBF.png)
+
+#### 2. 多项式核函数(Polynomial Kernel)
+
+![polynomial-kernel](../resources/KPCA/polynomial.png)
+
+#### 3. Sigmoid核(Sigmoid Kernel)
+
+![sigmoid-kernel](../resources/KPCA/sigmoid_kernel.png)
+
+## 三、效果
+
+### 1. K = 1，使用RBF核函数，参数γ=2
+
+![KPCA](../results/KPCA/gaussian_kenel.png)
+
+### 2. K = 1，使用RBF核函数，参数γ=0.5
+
+![KPCA](../results/KPCA/gaussian_kenel_2.png)
+
+### 3. K = 1，使用Polynomial核函数 
+
+![KPCA](../results/KPCA/polynomial.png)
+
+可以看到数据在原空间线性不可分，如果直接使用PCA降维的话必然导致数据类别的混叠，后续分类器性能下降。而使用KPCA将数据映射到再生核希尔伯特空间之后，
+数据线性可分，再在RKHS中使用PCA降维后的数据可以分离。但使用什么样的核函数、用什么样的参数还需要针对具体任务试验获得。
+
+## 四、总结
+核技巧是一套非常优美的技术，具有坚实的理论基础，但是目前仍不能对使用什么核函数、用什么参数给出一个明确的指导，逐渐被数据驱动的深度学习所取代，但核技巧蕴含的思想依然可以给当今机器学习算法设计很多启发。
+
+## 五、参考资料
+1. 周志华. 机器学习 : = Machine learning[M]. 清华大学出版社, 2016.(第十章)
+2. [https://blog.csdn.net/qq_27231343/article/details/51817866]()
---
+++ src/KPCA.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： 11360
+# datetime： 2021/3/14 12:18 
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_circles, make_moons
+
+
+class KPCA:
+    def __init__(self, k):
+        """
+        :param k: reduced dimension R^d -> R^k
+        """
+        self.reduced_dimension = k
+
+    def make_data(self):
+        self.X_data, self.Y_data = make_circles(n_samples=100, shuffle=True, noise=0.1, random_state=None, factor=0.2)
+        # self.X_data, self.Y_data = make_moons(100, noise=.04, random_state=0)
+        self.X_data = self.X_data - np.mean(self.X_data, 0)
+
+    def KPCA(self):
+        # kernel_function = lambda x1, x2: np.exp(-np.linalg.norm(x1 - x2) ** 2 * 0.5)
+        kernel_function = lambda x1, x2: (x1 @ x2 + 1) ** 3
+        kernel_matrix = self.construct_kernel_matrix(kernel_function)
+        # 中心化后的kernel matrix
+        num = self.X_data.shape[0]
+        K_star = kernel_matrix - 1 / num * kernel_matrix @ \
+                 np.mat(np.ones([num, num]))
+        eig_value, eig_vector = np.linalg.eig(K_star)
+
+        U = eig_vector[:, 0:self.reduced_dimension].real
+        print(U)
+        diag = np.diag(1 / np.sqrt(np.diag(U.T @ kernel_matrix @ U)))
+        print("diag = ", diag)
+        self.new_mat = diag @ U.T @ K_star
+
+    def construct_kernel_matrix(self, kernel_function=None):
+        if not kernel_function:
+            return
+        num = self.X_data.shape[0]
+        kernel_matrix = np.zeros([num, num])
+        for i in range(num):
+            for j in range(i, num):
+                kernel_matrix[i, j] = kernel_function(self.X_data[i, :],
+                                                      self.X_data[j, :])
+                kernel_matrix[j, i] = kernel_matrix[i, j]
+        return kernel_matrix
+
+    def result(self):
+        plt.subplot(121)
+        plt.title("Origin data")
+        one_index = np.where(self.Y_data == 1)
+        zero_index = np.where(self.Y_data == 0)
+        plt.scatter(self.X_data[one_index, 0], self.X_data[one_index, 1], c='r')
+        plt.scatter(self.X_data[zero_index, 0], self.X_data[zero_index, 1], c='b')
+
+        #######################################
+        plt.subplot(122)
+        plt.title("KPCA dimension=1 kernel=polynomial")
+        if self.reduced_dimension == 2:
+            for i in range(self.new_mat.shape[1]):
+                if self.Y_data[i] == 1:
+                    plt.scatter(self.new_mat[0, i],
+                                self.new_mat[1, i], c='r')
+                else:
+                    plt.scatter(self.new_mat[0, i],
+                                self.new_mat[1, i], c='b')
+        else:
+            for i in range(self.new_mat.shape[1]):
+                if self.Y_data[i] == 1:
+                    plt.scatter(self.new_mat[0, i], 0, c='r')
+                else:
+                    plt.scatter(self.new_mat[0, i], 0, c='b')
+        ax = plt.gca()
+        ax.axis("equal")
+        plt.show()
+
+
+if __name__ == "__main__":
+    a = KPCA(1)
+    a.make_data()
+    a.KPCA()
+    a.result()
+
+
+
+
+
