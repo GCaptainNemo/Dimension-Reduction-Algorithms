7397f95791787b0a0ab218ddb9e8225062007301
GCaptainNemo <1136003685@qq.com>
2021-03-16 02:26:00 +0800

    finish LDA and compare LDA with PCA

---
+++ docs/LDA.md
@@ -0,0 +1,61 @@
+# 线性判别分析
+## 一、介绍
+线性判别分析(Linear Discriminant Analysis, LDA)是一种监督式线性降维方法，也称为Fisher判别。LDA的思想非常朴素，给定训练集，将训练集投影到某个超平面上，
+使得同类样例的投影点尽可能接近，异类样例点尽量远离。对新样本点进行分类时，将其投影到该超平面上，再根据投影点的位置确定新样本的类别。因此，LDA本身还是一种分类算法。
+
+## 二、原理
+### 2.1 二分类
+给定训练集D = {(xi, yi)} i=1, 2, ..., N，yi∈{0, 1}。Xi, μi, Σi(i∈{0, 1})分别表示属于i标签的训练集集合，对应均值和协方差矩阵。若将数据投影到超平面w上，则
+中心投影坐标为w<sup>T</sup>μ<sub>0</sub>和w<sup>T</sup>μ<sub>1</sub>，协方差矩阵为w<sup>T</sup>Σ<sub>0</sub>w和w<sup>T</sup>Σ<sub>1</sub>w。
+
+为使投影到超平面上后类内紧密，类间远离，定义类内散度矩阵("within-class scatter matrix"):
+
+![within-class-matrix](../resources/LDA/lda_within.png)
+
+定义类间散度矩阵：
+
+![between-class-matrix](../resources/LDA/lda_between.png)
+
+称下式为关于S<sub>b</sub>和S<sub>w</sub>的广义瑞利熵：
+
+![generalized-rayleigh-quotient](../resources/LDA/generalized_rayleigh_quotient.png)
+
+而要实现类内紧密，类间原理的效果，可以将问题转化为如下优化函数：
+
+![optimizing](../resources/LDA/optimizing.png)
+
+由lagrange乘子法可得W的闭式解是S<sub>w</sub><sup>-1</sup>S<sub>b</sub>的d'个最大非0广义特征值对应特征向量组成的矩阵。
+将W视为一个投影矩阵，则相当于将样本投影(降维)到d’维空间。**注意**：d'≤ N - 1，因为S<sub>w</sub><sup>-1</sup>S<sub>b</sub>最多由N-1个非零广义特征值。
+
+### 2.2 多分类
+LDA多分类问题和二分类问题的优化形式和求解方式相同，唯一的区别在于类内散度矩阵S<sub>w</sub>和类间散度矩阵S<sub>b</sub>的定义。
+
+类内散度矩阵(within-class scatter matrix)是二分类中S<sub>w</sub>的一个自然推广：
+
+![within-class](../resources/LDA/lda_within_2.png)
+
+而类间散度矩阵(between-class scatter matrix)则需要用全局散度矩阵诱导出来：
+
+![between-class](../resources/LDA/lda_between_2.png)
+
+其中μ代表训练集中所有样本的均值。
+
+
+## 三、效果
+
+## 1. moon数据
+
+![moon](../results/LDA/moon.png)
+
+## 2. regression数据
+
+![regression](../results/LDA/regression.png)
+
+
+## 四、总结
+1. 与[PCA](PCA.md)中的结果进行对比可以发现，PCA没有考虑数据标签的信息，让数据投影后方差最大；而LDA则考虑了训练集数据的标签信息，让数据朝着类内紧密、类间分离的方向投影。
+2. LDA从贝叶斯决策理论的角度来看，当两类数据满足同先验、满足高斯分布且协方差矩阵相等时，LDA可达到最优分类。
+
+## 五、参考资料
+[1] 周志华. 机器学习 : = Machine learning[M]. 清华大学出版社, 2016.(第十章)
+
--- src/LDA.py
+++ src/LDA.py
@@ -2,3 +2,123 @@
 # -*- coding: utf-8 -*-
 # author： 11360
 # datetime： 2021/3/11 18:58 
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from sklearn.datasets import make_moons, make_regression
+
+
+class LDA:
+    def __init__(self, k):
+        """
+        :param k: reduced dimension R^d -> R^k
+        """
+        self.reduced_dimension = k
+
+    def make_data(self, regression=False):
+        if regression:
+            x, y = make_regression(n_samples=50, n_features=1,
+                                   n_targets=1, noise=1.5, random_state=1, bias=0)
+            y = np.array([y]).T
+            print(y.shape)
+            self.X_data = np.concatenate([x, y], axis=1)
+            new_X_data = self.X_data - np.array([[15, 13]])
+
+            self.X_data = np.concatenate([self.X_data, new_X_data], axis=0)
+            # self.X_data = self.X_data - np.mean(self.X_data, 0)
+            y = np.array([1 for _ in range(50)])
+            new_y = np.array([0 for _ in range(50)])
+            self.Y_data = np.concatenate([y, new_y], axis=0)
+            print(self.X_data.shape)
+        else:
+            self.X_data, self.Y_data = make_moons(100, noise=.04, random_state=0)
+
+    def cal_within(self):
+        """
+        calculate within-class scatter matrix
+        :return: Sw
+        """
+        class_name = np.unique(self.Y_data)
+        class_num = len(np.unique(self.Y_data))
+        attribute_dimension = self.X_data.shape[1]
+        Sw = np.zeros([attribute_dimension, attribute_dimension])
+        for i in range(class_num):
+            xi = self.X_data[np.where(self.Y_data == class_name[i])[0], :]
+            miui = np.array([np.mean(xi, axis=0)])
+            print(xi.shape)
+            Sw += xi.T @ xi
+            Sw -= xi.shape[0] * miui.T @ miui
+        return Sw
+
+    def cal_between(self, Sw):
+        """
+        calculate class-between scatter matrix
+        Sb = St - Sw
+        :return: Sb
+        """
+        St = self.X_data.T @ self.X_data
+        miu = np.array([np.mean(self.X_data, axis=0)])
+        St = St - self.X_data.shape[0] * miu.T @ miu
+        Sb = St - Sw
+        # miu0 = np.array([np.mean(self.X_data[np.where(self.Y_data == 0)[0], :], axis=0)])
+        # miu1 = np.array([np.mean(self.X_data[np.where(self.Y_data == 1)[0], :], axis=0)])
+        # print("miu0.shape = ", miu0.shape)
+        # Sb = (miu0 - miu1).T @(miu0 - miu1)
+        return Sb
+
+    def LDA(self):
+        Sw = self.cal_within()
+        Sb = self.cal_between(Sw)
+        eigval, eig_vec = scipy.linalg.eig(Sb, Sw)
+        eigval = eigval.real
+        index_sorted = np.argsort(eigval)[-self.reduced_dimension:]
+        self.Projective_matrix = eig_vec[:, index_sorted]
+        mat = np.mat(self.X_data).T
+        self.new_mat = self.Projective_matrix.T @ mat
+
+    def result(self, regression=False):
+        if regression == False:
+            translate_vector = [- self.Projective_matrix[1, 0] * 2, self.Projective_matrix[0, 0] * 2]
+        else:
+            translate_vector = [- self.Projective_matrix[1, 0] * 400, self.Projective_matrix[0, 0] * 400]
+
+        for i in range(self.new_mat.shape[1]):
+            if self.Y_data[i] == 1:
+                plt.scatter(self.new_mat[0, i] * self.Projective_matrix[0, 0] + translate_vector[0],
+                            self.new_mat[0, i] * self.Projective_matrix[1, 0] + translate_vector[1], c='r')
+            else:
+                plt.scatter(self.new_mat[0, i] * self.Projective_matrix[0, 0] + translate_vector[0],
+                            self.new_mat[0, i] * self.Projective_matrix[1, 0] + translate_vector[1], c='b')
+        one_index = np.where(self.Y_data == 1)
+        zero_index = np.where(self.Y_data == 0)
+        plt.scatter(self.X_data[one_index, 0], self.X_data[one_index, 1], c='r')
+        plt.scatter(self.X_data[zero_index, 0], self.X_data[zero_index, 1], c='b')
+        ax = plt.gca()
+        ax.axis("equal")
+        ax.annotate("",
+                    xy=(translate_vector[0], translate_vector[1]),
+                    xytext=(0, 0),
+                    arrowprops=dict(arrowstyle="->", color="k"))
+        if regression:
+            plt.text(translate_vector[0] * 0.9, translate_vector[1] * 0.95 + 10,
+                     r'projection', fontdict={'size': 8, 'color': 'k'})
+        else:
+            plt.text(translate_vector[0] * 0.95 + 0.05, translate_vector[1] * 0.95,
+                     r'projection', fontdict={'size': 8, 'color': 'k'})
+        plt.show()
+
+
+if __name__ == "__main__":
+    a = LDA(1)
+    a.make_data()
+    # a.make_data(True)
+
+    a.LDA()
+    a.result()
+    # a.result(True)
+
+
+
+
